aed76a4517d3d167b2b64a216319a333a9e13566
trsv-dev <119339792+trsv-dev@users.noreply.github.com>
2023-04-12 21:44:55 +0300

    Revert "Feature/comments"

--- api_yamdb/reviews/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def username_validator(value):
-    if value == 'me':
-        raise ValidationError(
-            'Имя пользователя "me" зарезервировано в системе'
-        )
