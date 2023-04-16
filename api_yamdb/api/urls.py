0574e345446655c7c2d958f186293bd4b8c153b9
trsv-dev <119339792+trsv-dev@users.noreply.github.com>
2023-04-16 18:30:54 +0300

    Revert "Revert "Revert "Authentification"""

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from rest_framework import routers
 
 # в строку ниже надо добавить вьюсет для Auth когда он будет готов
-from .views import (UserViewSet, CategoryViewSet, GenreViewSet,
-                    TitleViewSet, ReviewViewSet, CommentViewSet)
+from .views import (CategoryViewSet, GenreViewSet, TitleViewSet,
+                    UserViewSet, ReviewViewSet, CommentViewSet)
 
 router = routers.DefaultRouter()
 
@@ -11,18 +11,18 @@ router = routers.DefaultRouter()
 # пока Auth как заглушка
 
 # router.register(r'auth', Auth, basename='auth')
-router.register(r'groups', UserViewSet, basename='users')
-router.register(r'posts', CategoryViewSet, basename='categories')
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'genres', GenreViewSet, basename='genres')
 router.register(r'titles', TitleViewSet, basename='titles')
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet, basename='reviews')
+    ReviewViewSet, basename='reviews'
+)
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
-
 
 urlpatterns = [
     path('v1/', include(router.urls)),
---
+++ api_yamdb/reviews/management/commands/_load_cvs_func.py
@@ -0,0 +1,57 @@
+import csv
+import sys
+import time
+
+from api_yamdb.settings import BASE_DIR
+from reviews.models import (Category, Comment, Genre,
+                            Review, Title, User, GenreTitle)
+
+CSV_DATA = {
+
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    User: 'users.csv',
+    Title: 'titles.csv',
+    GenreTitle: 'genre_title.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv'
+}
+
+
+def sleep_period(sec):
+    for i in range(sec, 0, -1):
+        sys.stdout.write(str(i)+' ')
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+def load_func(self, *args, **options):
+
+    self.stdout.write(self.style.WARNING(
+        f'Перед загрузкой тестовых данных БД будет очищена. '
+        f'Чтобы отменить операцию импорта нажмите Ctrl + C'
+    ))
+    sleep_period(7)
+
+    for model, file_csv in CSV_DATA.items():
+
+        with open(
+                f'{BASE_DIR}/static/data/{file_csv}',
+                'r', encoding='utf-8'
+        ) as file:
+            data = csv.DictReader(file)
+            lst = []
+
+            for row in data:
+                if 'category' in row:
+                    row['category_id'] = row.pop('category')
+                elif 'author' in row:
+                    row['author_id'] = row.pop('author')
+                lst.append(model(**row))
+
+            # очищаем базу от старых данных перед импортом
+            model.objects.all().delete()
+            # наполняем новыми данными
+            model.objects.bulk_create(lst)
+
+            print(f'Файл {file_csv} загружен')
