7bbc01593c1e93e659a94f5cb9948e97af00a0b7
trsv-dev <tarasov.itc@yandex.ru>
2023-04-16 18:21:15 +0300

    Второе разрешение конфликтов в settings.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import (
-    User, Category, Review, Comment, Genre, Title)
+from .models import (User, Category, Review, Comment,
+                     Genre, Title, GenreTitle)
 
 
 @admin.register(User)
@@ -19,7 +19,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name', 'slug')
     search_fields = ('id', 'name', 'slug')
     list_filter = ('name', 'slug')
-    prepopulated_fields = {"slug": ("name",)}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Genre)
@@ -28,7 +28,7 @@ class GenreAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name', 'slug')
     search_fields = ('id', 'name', 'slug')
     list_filter = ('name', 'slug')
-    prepopulated_fields = {"slug": ("name",)}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Title)
@@ -39,12 +39,20 @@ class TitleAdmin(admin.ModelAdmin):
     list_filter = ('genre', 'category')
 
 
+@admin.register(GenreTitle)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'genre_id', 'title_id')
+    list_display_links = ('id', 'genre_id', 'title_id')
+    search_fields = ('id', 'genre_id', 'title_id')
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title_id', 'author', 'text', 'score', 'pub_date')
+    list_display = ('id', 'title_id', 'author', 'text', 'score', 'pub_date', 'rating')
     list_display_links = ('id', 'title_id', 'author', 'text', 'score', 'pub_date')
     search_fields = ('author__username', 'text', 'pub_date')
     list_filter = ('pub_date',)
+    readonly_fields = ('rating',)
 
 
 @admin.register(Comment)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,24 +3,27 @@ from datetime import datetime
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-
+from django.db.models import Avg
 from reviews.validators import username_validator
-
-USER = 'Пользователь'
-MODERATOR = 'Модератор'
-ADMIN = 'Администратор'
-
-ROLE_CHOICES = (
-    (USER, 'Пользователь'),
-    (MODERATOR, 'Модератор'),
-    (ADMIN, 'Администратор')
-)
+from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
+
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+    ROLE_CHOICES = [
+        (ADMIN, 'Администратор'),
+        (MODERATOR, 'Модератор'),
+        (USER, 'Пользователь'),
+    ]
+
     username = models.CharField(
         validators=(username_validator,),
-        max_length=200,
+        max_length=150,
         unique=True,
         blank=False,
         null=False,
@@ -28,7 +31,7 @@ class User(AbstractUser):
         help_text='Имя пользователя'
     )
     email = models.EmailField(
-        max_length=200,
+        max_length=254,
         unique=True,
         blank=False,
         null=False,
@@ -41,13 +44,13 @@ class User(AbstractUser):
         help_text='Напишите пару фактов из биографии'
     )
     first_name = models.CharField(
-        max_length=200,
+        max_length=150,
         blank=True,
         verbose_name='Имя',
         help_text='Введите имя'
     )
     last_name = models.CharField(
-        max_length=200,
+        max_length=150,
         blank=True,
         verbose_name='Фамилия',
         help_text='Введите фамилию'
@@ -56,7 +59,7 @@ class User(AbstractUser):
     role = models.CharField(
         choices=ROLE_CHOICES,
         default=USER,
-        max_length=50,
+        max_length=150,
         blank=True,
         verbose_name='Роль',
         help_text='Выберите роль'
@@ -77,22 +80,36 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=100,
+        max_length=256,
         verbose_name='Категория',
         help_text='Укажите название категории'
     )
     slug = models.SlugField(
         unique=True,
-        max_length=100,
+        validators=(
+            RegexValidator(
+                regex=r'^[-a-zA-Z0-9_]+$',
+                message='Недопустимый символ в slug'
+            ),
+        ),
+        max_length=50,
         verbose_name='URL',
         help_text='Укажите URL категории'
     )
 
     class Meta:
-        verbose_name = 'Категория',
+        verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
@@ -107,13 +124,18 @@ class Genre(models.Model):
     )
     slug = models.SlugField(
         unique=True,
-        max_length=100,
+        validators=(
+            RegexValidator(
+                regex=r'^[-a-zA-Z0-9_]+$',
+                message='Недопустимый символ в slug'
+            ),
+        ),
         verbose_name='URL',
         help_text='Укажите URL жанра'
     )
 
     class Meta:
-        verbose_name = 'Жанр',
+        verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
 
     def __str__(self):
@@ -122,7 +144,7 @@ class Genre(models.Model):
 
 class Title(models.Model):
     name = models.CharField(
-        max_length=200,
+        max_length=100,
         verbose_name='Название',
         help_text='Укажите название'
     )
@@ -147,6 +169,8 @@ class Title(models.Model):
     )
     genre = models.ManyToManyField(
         Genre,
+        blank=True,
+        through='GenreTitle',
         related_name='titles',
         verbose_name='Жанр',
         help_text='Укажите жанр произведения'
@@ -159,11 +183,37 @@ class Title(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Произведение',
+        verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
+
+    def update_rating(self):
+        self.rating = self.reviews.aggregate(Avg("score"))["score__avg"] or 0
+        self.save()
 
     def __str__(self):
         return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        verbose_name='Жанр',
+        help_text='Укажите жанр'
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение',
+        help_text='Укажите произведение'
+    )
+
+    class Meta:
+        verbose_name = '"Связь" жанр/произведение'
+        verbose_name_plural = '"Связь" жанры/произведения'
+
+    def __str__(self):
+        return f'{self.title} {self.genre}'
 
 
 class Review(models.Model):
@@ -192,7 +242,7 @@ class Review(models.Model):
         help_text='Выберите автора'
     )
     score = models.IntegerField(
-        verbose_name='Рейтинг',
+        verbose_name='Оценка',
         help_text='Оцените от 1 до 10',
         validators=(
             MinValueValidator(1, message='Оценка должна быть от 1 до 10'),
@@ -202,6 +252,18 @@ class Review(models.Model):
     pub_date = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата публикации'
+    )
+
+    rating = models.DecimalField(
+        default=0,
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Рейтинг',
+        help_text='Рейтинг произведения.',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        )
     )
 
     class Meta:
@@ -215,6 +277,13 @@ class Review(models.Model):
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
         ordering = ('-pub_date',)
+
+    def average_rating(self) -> float:
+        return Review.objects.aggregate(Avg("score"))['score__avg']
+
+    def save(self, *args, **kwargs):
+        self.rating = self.average_rating()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Отзыв {self.author} на "{self.title}"'
