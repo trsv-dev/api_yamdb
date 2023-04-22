8b4011f2a0721c30ef486a2c46ee7a1a89c9925d
trsv-dev <tarasov.itc@yandex.ru>
2023-04-22 19:28:58 +0300

    After Codereview 2.4. Автор - Василе. Изменение ReviewSerializer. Мелкие исправления.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -57,28 +58,39 @@ class TitleReadSerializer(serializers.ModelSerializer):
         )
 
 
+class TitleDefault:
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        view = serializer_field.context['view']
+        title_id = view.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     """ Сериализатор для работы с Review."""
 
-    author = serializers.SlugRelatedField(
-        slug_field='username', read_only=True,
-        default=serializers.CurrentUserDefault()
+    title = serializers.HiddenField(
+        default=TitleDefault()
     )
-    title = serializers.PrimaryKeyRelatedField(
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
         read_only=True,
     )
 
     class Meta:
         model = Review
-        fields = ('id', 'text', 'author', 'score', 'pub_date', 'title')
-
+        fields = ('id', 'text', 'score', 'pub_date', 'author', 'title')
         validators = [
             UniqueTogetherValidator(
                 queryset=Review.objects.all(),
-                fields=['author', 'title']
+                fields=('title', 'author'),
+                message='Вы уже написали отзыв для этого произведения.'
             )
         ]
-    
+
 
 class CommentSerializer(serializers.ModelSerializer):
     """ Сериализатор для работы с Comments."""
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -84,14 +84,16 @@ class ReviewViewSet(viewsets.ModelViewSet):
     pagination_class = LimitOffsetPagination
     permission_classes = ((IsAuthor | IsModerator | IsAdmin),)
 
-    def perform_create(self, serializer):
+    def get_title(self):
         title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
+        return get_object_or_404(Title, id=title_id)
+
+    def perform_create(self, serializer):
+        title = self.get_title()
         serializer.save(author=self.request.user, title=title)
 
     def get_queryset(self):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
+        title = self.get_title()
         return title.reviews.all()
 
 
@@ -107,12 +109,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = ((IsAuthor | IsModerator | IsAdmin),)
 
+    def get_review(self):
+        review_id = self.kwargs.get("review_id")
+        return get_object_or_404(Review, id=review_id)
+
     def get_queryset(self):
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id)
+        review = self.get_review()
         return review.comments.all()
 
     def perform_create(self, serializer):
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id)
+        review = self.get_review()
         serializer.save(author=self.request.user, review=review)
